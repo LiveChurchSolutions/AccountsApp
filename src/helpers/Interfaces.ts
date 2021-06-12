27823e7fb4a56b4ae0c236bdcffa7b3cae778e0f
Jeremy Zongker <jeremy@trilitech.com>
2021-06-12 02:29:31 +0000

    Switch to light and dark horizontal logos

--- src/helpers/Interfaces.ts
+++
@@ -1,4 +0,0 @@
-export * from "../appBase/interfaces";
-
-export interface PaymentGatewaysInterface { id?: string, churchId?: string, provider?: string, publicKey?: string, privateKey?: string }
-//export interface IChurchAppearance { primaryColor?: string, primaryContrast?: string, secondaryColor?: string, secondaryContrast?: string, logoLight?: string, logoDark?: string }
--- src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "../appBase/helpers"
-export { EnvironmentHelper } from "./EnvironmentHelper"
-export { Helper } from "./Helper"
-export { ArrayHelper } from "./ArrayHelper"
-export * from "./Interfaces";
-export { UniqueIdHelper } from "../appBase/helpers";
-export { PersonHelper } from "./PersonHelper"
