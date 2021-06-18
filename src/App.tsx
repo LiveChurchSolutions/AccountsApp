13fd2909b41df5cda5915cce375be918259dc0df
pranavmalvawala <pranavmalvawala@users.noreply.github.com>
2021-06-18 03:45:43 +0000

    Apply linting

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,7 @@ import { UserProvider } from "./UserContext"
 import { Routing } from "./Routing";
 import { CookiesProvider } from "react-cookie"
 
-
-
-
-
-
-
-
-
-
-            console.log("this should work")
-
-
-
-
-
-
+console.log("this should work")
 
 const App: React.FC = () => (
   <UserProvider>
